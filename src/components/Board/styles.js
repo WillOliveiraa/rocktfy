0f1deaba8c4ee8f6437a0070cf41b8a88312a8f8
Willian <willian@processinformatica.com.br>
2020-06-29 15:53:56 -0300

    Add app

---
+++ src/components/Board/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    padding: 30px 0;
+    height: calc(100% - 80px); /* Só funciona pq no global foi setado que o height e 100% */
+
+    
+`;
